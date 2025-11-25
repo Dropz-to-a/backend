4e0f2f088a48f34b261e8f5596bdd0c65132e6da
alvin081105 <ca081105@gmail.com>
2025-11-25 16:48:37 +0900

    EmployeeService코드 최적화

--- src/main/java/com/jobmanager/job_manager/service/EmployeeService.java
+++ src/main/java/com/jobmanager/job_manager/service/EmployeeService.java
@@ -13,14 +13,11 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-    /** 회사가 직원 등록 */
     public Employee assignEmployee(Long companyId, Long employeeId) {
 
-        // 중복 소속 체크
         employeeRepository.findByCompanyIdAndEmployeeId(companyId, employeeId)
                 .ifPresent(e -> { throw new IllegalArgumentException("이미 이 회사 소속입니다."); });
 
-        // 새 등록
         Employee employee = Employee.builder()
                 .companyId(companyId)
                 .employeeId(employeeId)
