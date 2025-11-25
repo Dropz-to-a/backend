1f9b9b2d9a72bc4b2e9e8395ea7fca649bccb612
alvin081105 <ca081105@gmail.com>
2025-11-25 16:24:50 +0900

    fix: 로직 흐름 오류 수정

--- src/main/java/com/jobmanager/job_manager/repository/EmployeeRepository.java
+++ src/main/java/com/jobmanager/job_manager/repository/EmployeeRepository.java
@@ -3,8 +3,13 @@ package com.jobmanager.job_manager.repository;
 import com.jobmanager.job_manager.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    /** 회사에 특정 직원이 소속되어 있는지 확인 */
     boolean existsByCompanyIdAndEmployeeId(Long companyId, Long employeeId);
 
+    /** 회사 + 직원 조합으로 row 조회 */
+    Optional<Employee> findByCompanyIdAndEmployeeId(Long companyId, Long employeeId);
 }
